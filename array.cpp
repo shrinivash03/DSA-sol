6040317ff33a318aefbcbdf9304f81731d19e24c
shrinivash03 <nivaskvs844102@gmail.com>
2024-04-11 21:56:10 +0530

    hii

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int arr[n];
+cout<<"enter the element of the array:"<<endl;
+for(int i=0;i<n;i++)
+{
+    cin>>arr[i];
+}
+cout<<"the element of array is:"<<endl;
+for(int i=0;i<n;i++)
+{
+    cout<<arr[i];
+}
+}
